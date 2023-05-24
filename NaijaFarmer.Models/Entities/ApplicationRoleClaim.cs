73d4a72371530151e3c525e6c4dc2dc7171a97c5
Joshua Eze <ezejoshuac@gmail.com>
2023-05-24 16:52:02 +0100

    Added custom identity user management entities

---
+++ NaijaFarmer.Models/Entities/ApplicationRoleClaim.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NaijaFarmer.Models.Entities
+{
+    public class ApplicationRoleClaim : IdentityRoleClaim<string>
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+        public ApplicationRole Role { get; set; }
+    }
+}
---
+++ NaijaFarmer.Models/Enums/UserType.cs
@@ -0,0 +1,22 @@
+﻿namespace NaijaFarmer.Models.Enums
+{
+    public enum UserType
+    {
+        Customer = 1,
+        Farmer,
+        Admin
+    }
+    public static class UserTypeExtension
+    {
+        public static string? ToStringValue(this UserType userType)
+        {
+            return userType switch
+            {
+                UserType.Customer => "user",
+                UserType.Farmer => "farmer",
+                UserType.Admin => "admin",
+                _ => null
+            };
+        }
+    }
+}
